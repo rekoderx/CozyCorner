81e44befad497d59ef9d57ca06301378de105907
Roberto Enea <rekoderx@gmail.com>
2021-05-28 10:45:37 +0200

    Final v1.0

--- main.js
+++ main.js
@@ -27,24 +27,24 @@ $('.review-wrapper').slick({
     nextArrow: $('.next'),
     prevArrow: $('.prev'),
     responsive: [
-        // {
-        //     breakpoint: 1600,
-        //     settings: {
-        //       slidesToShow: 3,
-        //       slidesToScroll: 1,
-        //       infinite: true,
-        //       dots: true
-        //     }
-        // },
-        // {
-        //   breakpoint: 1024,
-        //   settings: {
-        //     slidesToShow: 1,
-        //     slidesToScroll: 1,
-        //     infinite: true,
-        //     dots: true
-        //   }
-        // },
+        {
+            breakpoint: 1600,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
         {
             breakpoint: 768,
             settings: {
